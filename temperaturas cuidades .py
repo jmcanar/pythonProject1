3ce1fb11552c3859ce84931e229279c3962d2249
jmcanar <jm.canarj@uea.edu.ec>
2024-03-09 17:48:16 -0500

    prueba

--- temperaturas cuidades .py
+++ temperaturas cuidades .py
@@ -1,6 +1,8 @@
+# Desarrollo de Función para Calcular Temperaturas Promedio en Python
+#DEFINIMOS LA FUNCION
 def calcular_temperatura_promedio(datos):
     temperaturas_ciudades = {}
-
+#REAlizAMOS UN RECORRIDO EN LA MATRIZ  EN NUETRO ARREGLO CON BUCLE FOR
     for ciudad, semanas in datos.items():
         suma_temperaturas = 0
         total_semanas = len(semanas)
@@ -14,14 +16,17 @@ def calcular_temperatura_promedio(datos):
     return temperaturas_ciudades
 
 
-# Ejemplo de uso
+# DATOS DE TEMPERATURAS DE DIFERENTES CUIDADES
 datos = {
     "Quito": [22, 23, 21, 20],
     "Guayaquil": [28, 30, 29, 27],
     "Cuenca": [20, 18, 19, 21]
 }
-
+#LLAMADA A LA FUNCION
 temperaturas_promedio = calcular_temperatura_promedio(datos)
-
+#IMPRIMIMOS EL RESULTADO
 for ciudad, temperatura in temperaturas_promedio.items():
     print(f"La temperatura promedio en {ciudad} es de {temperatura} grados Celsius.")
+
+
+#NOMBRE: MARCELO CAÑAR

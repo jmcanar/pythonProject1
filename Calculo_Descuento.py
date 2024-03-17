24ee4fe413ba18fdf3b9df1b521a045951c4a2dd
jmcanar <jm.canarj@uea.edu.ec>
2024-03-17 11:26:08 -0500

    prueba

--- Calculo_Descuento.py
+++ Calculo_Descuento.py
@@ -3,10 +3,19 @@ def calcular_descuento(precio_original, porcentaje_descuento):
     precio_final = precio_original - descuento
     return precio_final
 # valores
-precio_inicial = 100 #valor de la compra
+precio_inicial = 1000 #valor de la compra
 descuento_porcentaje = 10 #porcentaje de descuento
+descuento_porcentaje2 = 10
 #llamada de la funcion
 precio_con_descuento = calcular_descuento(precio_inicial, descuento_porcentaje)
 print(f"El precio original es: ${precio_inicial}")
 print(f"El descuento aplicado es del {descuento_porcentaje}%")
-print(f"El precio final con descuento es: ${precio_con_descuento}")
+print(f"El precio final con descuento uno es: ${precio_con_descuento}")
+#llamamos nuevamente a la funcion
+precio_con_descuento2 = calcular_descuento(precio_con_descuento,descuento_porcentaje2)
+print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+print("tiene una promocion de descuento del 10% del valor final")
+print(f"El precio final con descuento dos  es: ${precio_con_descuento2}")
+
+
+#Marcelo Canar

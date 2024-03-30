ab891a646695d8eba66c90ad2f57c79ac2e6b9ef
jmcanar <jm.canarj@uea.edu.ec>
2024-03-30 11:05:13 -0500

    prueba

--- my_notes.txt.py
+++ my_notes.txt.py
@@ -1,11 +1,14 @@
 # Creación del archivo my_notes.txt y escritura de notas personales
-with open('my_notes.txt', 'w') as file:
-    file.write("Nota 1: hola mi nombre es.\n")
-    file.write("Nota 2: marcelo cañar.\n")
-    file.write("Nota 3: cual es su nombre.\n")
+with open("my_notes.txt", "w") as archivo:
+    archivo.write("mi nombre es\n")
+    archivo.write("marcelo cañar\n")
+    archivo.write("cual es su nombre\n")
 
 # Lectura del archivo my_notes.txt y mostrar su contenido línea por línea
-with open('my_notes.txt', 'r') as file:
-    print("Contenido del archivo my_notes.txt:")
-    for line in file.readlines():
-        print(line.strip())  # Strip para eliminar cualquier espacio en blanco o caracteres de nueva línea
+
+with open("my_notes.txt", "r") as archivo:
+    lineas = archivo.readlines()
+    for linea in lineas:
+        print(linea.strip())  # Strip para eliminar cualquier espacio en blanco o caracteres de nueva línea
+# Cierre de archivos
+archivo.close()
